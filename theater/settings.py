297131dc4b7241922ad2eabf1fb033a2be5c65ae
lujainb <lujain@sitech.me>
2022-08-16 22:09:04 +0300

    allowed hosts

--- theater/settings.py
+++ theater/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vboxl*4&%7*)c!p9al&a2cfa&q+9#n$fwvu=#&_9%e+b&mu#^j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["167.71.43.17", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["inadtheater.ps", "127.0.0.1", "localhost"]
 
 # Application definition
 
