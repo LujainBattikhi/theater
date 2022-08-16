910f93ab3cc70be9b6f7d42921fe03d5c0409887
lujainb <lujain@sitech.me>
2022-08-17 00:55:24 +0300

    DEBUG

--- theater/settings.py
+++ theater/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vboxl*4&%7*)c!p9al&a2cfa&q+9#n$fwvu=#&_9%e+b&mu#^j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["inadtheater.ps", "127.0.0.1", "localhost"]
 
